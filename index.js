c6216b3d21c3d4042254486eb7a8521e661cd0f9
Fernando Martinez Jr <fernando.martinez.jr.59@gmail.com>
2022-04-28 17:05:53 -0400

    questions complete

--- index.js
+++ index.js
@@ -7,33 +7,43 @@ const promptUser = () => {
   return inquirer.prompt([
     {
       type: 'input',
-      name: 'name',
-      message: 'What is your name?',
+      name: 'Project Title',
+      message: 'What is your Projects Title',
     },
     {
-      type: 'input',
-      name: 'location',
-      message: 'Where are you from?',
+      type: 'editor',
+      name: 'descriptionOne',
+      message: 'What was your motivation?',
     },
     {
-      type: 'input',
-      name: 'hobby',
-      message: 'What is your favorite hobby?',
+      type: 'editor',
+      name: 'descriptionTwo',
+      message: 'Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")?',
     },
     {
-      type: 'input',
-      name: 'food',
-      message: 'What is your favorite food?',
+      type: 'editor',
+      name: 'descriptionThree',
+      message: 'What problem does it solve?',
     },
     {
-      type: 'input',
-      name: 'github',
-      message: 'Enter your GitHub Username',
+      type: 'editor',
+      name: 'descriptionFour',
+      message: 'What did you learn?',
     },
     {
-      type: 'input',
-      name: 'linkedin',
-      message: 'Enter your LinkedIn URL.',
+      type: 'editor',
+      name: 'Installation',
+      message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
     },
+    {
+        type: 'editor',
+        name: 'Usage',
+        message: 'Provide instructions and examples for use. Include screenshots as needed. To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax: ```md ![alt text](assets/images/screenshot.png)```',
+      },
+      {
+        type: 'editor',
+        name: 'Credits',
+        message: 'List your collaborators, if any, with links to their GitHub profiles. If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section. If you followed tutorials, include links to those here as well',
+      },
   ]);
 };
